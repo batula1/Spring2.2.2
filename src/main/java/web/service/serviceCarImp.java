f590048ca439f58cfe5e20a3e6ad56cd71a3fc20
Albert Batula <batula227@gmail.com>
2023-11-27 16:05:48 +0300

    push

---
+++ src/main/java/web/service/serviceCarImp.java
@@ -0,0 +1,28 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.DAO.CarDao;
+import web.model.Car;
+
+import java.util.List;
+
+@Service
+public class serviceCarImp implements serviceCar {
+
+    private CarDao carDao;
+
+    @Autowired
+    public serviceCarImp(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
+    @Override
+    public List<Car> getCars(Integer count) {
+        if (count != null){
+            return carDao.getCars(count);
+        }else {
+            return carDao.getCars();
+        }
+    }
+}
